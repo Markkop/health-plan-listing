ca602dd2b189f36fc6b8fea0216d445389fc0dbf
Marcelo "Mark" Kopmann <marcelokopmann@gmail.com>
2021-06-23 15:57:59 -0300

    refac: form data structure

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,28 +3,28 @@
     <form>
       <Select
         type="state"
-        :options="states"
-        :isLoading="isLoadingStates"
+        :options="state.list"
+        :isLoading="state.isLoading"
         @change="setSelectedOption"
       />
       <Select
         type="city"
-        :options="cities"
-        :isLoading="isLoadingCities"
+        :options="city.list"
+        :isLoading="city.isLoading"
         :hasRequiredSelection="Boolean(selectedState)"
         @change="setSelectedOption"
       />
       <Select
         type="profession"
-        :options="professions"
-        :isLoading="isLoadingProfessions"
+        :options="profession.list"
+        :isLoading="profession.isLoading"
         :hasRequiredSelection="Boolean(selectedCity)"
         @change="setSelectedOption"
       />
       <Select
         type="entity"
-        :options="entities"
-        :isLoading="isLoadingEntities"
+        :options="entity.list"
+        :isLoading="entity.isLoading"
         :hasRequiredSelection="Boolean(selectedProfession)"
         @change="setSelectedOption"
       />
@@ -42,78 +42,106 @@ export default {
   },
   data() {
     return {
-      cities: [],
-      states: [],
-      professions: [],
-      entities: [],
-      selectedCity: null,
-      selectedState: null,
-      selectedProfession: null,
-      selectedEntity: null,
-      isLoadingCities: false,
-      isLoadingStates: false,
-      isLoadingProfessions: false,
-      isLoadingEntities: false,
+      city: {
+        list: [],
+        selected: null,
+        isLoading: false,
+      },
+      state: {
+        list: [],
+        selected: null,
+        isLoading: false,
+      },
+      profession: {
+        list: [],
+        selected: null,
+        isLoading: false,
+      },
+      entity: {
+        list: [],
+        selected: null,
+        isLoading: false,
+      }
+    }
+  },
+  computed: {
+    selectedCity() {
+      return this.city.selected
+    },
+    selectedState() {
+      return this.state.selected
+    },
+    selectedProfession() {
+      return this.profession.selected
+    },
+    selectedEntity() {
+      return this.entity.selected
     }
   },
   watch: {
     async selectedState() {
-      const stateId = this.selectedState.id
+      const stateId = this.state.selected.id
       const cities = await this.getCities(stateId)
       this.setCities(cities)
-      this.selectedCity = null
+      this.city.selected = null
     },
     async selectedCity() {
       this.setProfessions([])
-      this.selectedProfession = null
-      const cityName = this.selectedCity.nome
-      const stateInitials =  this.selectedState.sigla
+      this.profession.selected = null
+      if (!this.city.selected) {
+        return
+      }
+      const cityName = this.city.selected.nome
+      const stateInitials =  this.state.selected.sigla
       const professions = await this.getProfessions(stateInitials, cityName)
       this.setProfessions(professions)
     },
     async selectedProfession() {
       this.setEntities([])
-      this.selectedEntity = null
-      const cityName = this.selectedCity.nome
-      const stateInitials =  this.selectedState.sigla
-      const profession = this.selectedProfession.nome
+      this.entity.selected = null
+      if (!this.profession.selected) {
+        return
+      }
+      const cityName = this.city.selected.nome
+      const stateInitials =  this.state.selected.sigla
+      const profession = this.profession.selected.nome
       const entities = await this.getEntities(stateInitials, cityName, profession)
       this.setEntities(entities)
     }
   },
   methods: {
-    async fetchAndSetLoading(url, loadingPropertyName) {
-      this[loadingPropertyName] = true
+    async fetchAndSetLoading(url, type) {
+      this[type].isLoading = true
       const response = await fetch(url)
       const cities = await response.json()
-      this[loadingPropertyName] = false
+      this[type].isLoading = false
       return cities
     },
     async getCities(stateId) {
       const citiesUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateId}/municipios`
-      return this.fetchAndSetLoading(citiesUrl, 'isLoadingCities')
+      return this.fetchAndSetLoading(citiesUrl, 'city')
     },
     async getStates() {
       const statesUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-      return this.fetchAndSetLoading(statesUrl, 'isLoadingStates')
+      return this.fetchAndSetLoading(statesUrl, 'state')
     },
     async getProfessions(stateInitials, cityName) {
       const cityNameUtf8 = Buffer.from(cityName, 'utf-8');
       const professionsUrl = `http://lb-aws-1105894158.sa-east-1.elb.amazonaws.com/profissao/${stateInitials}/${cityNameUtf8}?api-key=ddd70c32-fc98-4618-b494-a9698f824353`
-      return this.fetchAndSetLoading(professionsUrl, 'isLoadingProfessions')
+      return this.fetchAndSetLoading(professionsUrl, 'profession')
     },
     async getEntities(stateInitials, cityName, profession) {
       const cityNameUtf8 = Buffer.from(cityName, 'utf-8');
       const entitiesUrl = `http://lb-aws-1105894158.sa-east-1.elb.amazonaws.com/entidade/${profession}/${stateInitials}/${cityNameUtf8}?api-key=4b94dba2-5524-4632-939b-92df1c50a645`
-      return this.fetchAndSetLoading(entitiesUrl, 'isLoadingEntities')
+      return this.fetchAndSetLoading(entitiesUrl, 'entity')
     },
     async setStates(states) {
       this.sortByName(states)
-      this.states = states
+      this.state.list = states
     },
     async setCities(cities) {
       this.sortByName(cities)
-      this.cities = cities
+      this.city.list = cities
     },
     async setProfessions(professions) {
       const mappedProfessions = professions.map(({ profissao }) => ({
@@ -121,7 +149,7 @@ export default {
         nome: profissao
       }))
       this.sortByName(mappedProfessions)
-      this.professions = mappedProfessions
+      this.profession.list = mappedProfessions
     },
     async setEntities(entities) {
       const mappedEntities = entities.map(({ NomeFantasia, RazaoSocial }) => ({
@@ -129,24 +157,24 @@ export default {
         nome: `${NomeFantasia} - ${RazaoSocial}`
       }))
       this.sortByName(mappedEntities)
-      this.entities = mappedEntities
+      this.entity.list = mappedEntities
     },
-    sortByName(items) {
-      items.sort((itemA, itemB) => itemA.nome.localeCompare(itemB.nome))
+    sortByName(list) {
+      list.sort((itemA, itemB) => itemA.nome.localeCompare(itemB.nome))
     },
     setSelectedOption(type, selectedOption) {
       switch (type) {
         case 'city':
-          this.selectedCity = selectedOption
+          this.city.selected = selectedOption
           break;
         case 'state':
-          this.selectedState = selectedOption
+          this.state.selected = selectedOption
           break;
         case 'profession':
-          this.selectedProfession = selectedOption
+          this.profession.selected = selectedOption
           break;
         case 'entity':
-          this.selectedEntity = selectedOption
+          this.entity.selected = selectedOption
           break;
         default:
           break;
