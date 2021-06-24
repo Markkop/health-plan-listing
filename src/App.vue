881175a28cec36cb336d4f8ce7f942e8dcc91b73
Marcelo "Mark" Kopmann <marcelokopmann@gmail.com>
2021-06-24 11:41:29 -0300

    feat: header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <Home/>
+  <div id="app">
+    <Header/>
+    <Home/>
+  </div>
 </template>
 
 <script>
 import Home from './components/Home';
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-    Home
+    Home,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header id="page-header">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+  #page-header {
+    height: 30px;
+    border-bottom: 2px solid black;
+  }
+</style>
