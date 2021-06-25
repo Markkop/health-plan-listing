3f3a488d06a90107f71e64a7720c4f41b593b89a
Marcelo "Mark" Kopmann <marcelokopmann@gmail.com>
2021-06-24 23:24:54 -0300

    fix: mongo collection

--- server/src/utils/mongoose.js
+++ server/src/utils/mongoose.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
  */
 export function connectMongoose () {
   if (mongoose.connection.readyState === 0) {
-    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/test?retryWrites=true&w=majority`, {
+    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/healthplans?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
